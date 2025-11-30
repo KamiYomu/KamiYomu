cbc4ecf4fb59f7ce3fdf88fd92ff2babf46288f2
Marcos Costa <marcos.lcosta@live.com>
2025-11-30 10:38:04 -0500

    cancel execution of a job when lock is unavailable, add required fonts to questpdf (#83)

---
+++ src/KamiYomu.Web/Worker/Attributes/MangaCancelOnFailAttribute.cs
@@ -0,0 +1,93 @@
+﻿using Hangfire.Common;
+using Hangfire.States;
+using Hangfire.Storage;
+using KamiYomu.Web.Infrastructure.Contexts;
+
+namespace KamiYomu.Web.Worker.Attributes
+{
+    public class MangaCancelOnFailAttribute : JobFilterAttribute, IApplyStateFilter, IDisposable
+    {
+        public string CancelReason { get; }
+
+        private readonly string _libraryIdParameterName;
+        private readonly IServiceScope _scope;
+        private readonly ILogger<ChapterCancelOnFailAttribute> _logger;
+        private bool disposedValue;
+
+        public MangaCancelOnFailAttribute(string libraryIdParameterName, string cancelReason = "The number of attempts was exceeded")
+        {
+            CancelReason = cancelReason;
+            _libraryIdParameterName = libraryIdParameterName;
+            _scope = AppOptions.Defaults.ServiceLocator.Instance.CreateScope();
+            _logger = _scope.ServiceProvider.GetRequiredService<ILogger<ChapterCancelOnFailAttribute>>();
+        }
+
+        public void OnStateApplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
+        {
+            var oldState = context.OldStateName;
+            var newState = context.NewState?.Name;
+
+            if (oldState == ProcessingState.StateName &&
+                newState == FailedState.StateName)
+            {
+                var args = context.BackgroundJob.Job.Args;
+                var method = context.BackgroundJob.Job.Method;
+                var parameters = method.GetParameters();
+
+                int index = Array.FindIndex(parameters, p => p.Name == _libraryIdParameterName);
+
+                if (index == -1)
+                {
+                    _logger.LogError(
+                        "MangaCancelOnFail: Parameter '{Parameter}' not found for job {JobId}.",
+                        _libraryIdParameterName, context.BackgroundJob.Id);
+                    return;
+                }
+
+                if(args[index] is Guid libraryId)
+                {
+                    var jobId = context.BackgroundJob.Id;
+
+                    var dbContext = _scope.ServiceProvider.GetRequiredService<DbContext>();
+
+                    var library = dbContext.Libraries.FindById(libraryId);
+
+                    using var libDbContext = library.GetDbContext();
+
+                    var downloadChapter = libDbContext.MangaDownloadRecords.FindOne(p => p.BackgroundJobId == jobId);
+
+                    if (downloadChapter != null)
+                    {
+                        downloadChapter.Cancelled(CancelReason);
+                        libDbContext.MangaDownloadRecords.Update(downloadChapter);
+                    }
+                }
+
+                
+            }
+        }
+
+        public void OnStateUnapplied(ApplyStateContext context, IWriteOnlyTransaction transaction)
+        {
+            // Not needed
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _scope.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- src/KamiYomu.Web/Worker/Interfaces/IChapterDownloaderJob.cs
+++ src/KamiYomu.Web/Worker/Interfaces/IChapterDownloaderJob.cs
@@ -8,8 +8,9 @@ namespace KamiYomu.Web.Worker.Interfaces
     public interface IChapterDownloaderJob
     {
         [Queue("{0}")]
-        [PerKeyConcurrency("crawlerId")]
         [DisplayName("Down Chapter {5}")]
+        [PerKeyConcurrency("crawlerId")]
+        [ChapterCancelOnFail("libraryId")]
         Task DispatchAsync(string queue, Guid crawlerId, Guid libraryId, Guid mangaDownloadId, Guid chapterDownloadId, string title, PerformContext context, CancellationToken cancellationToken);
     }
 }
